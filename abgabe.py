d0064f1a347f2160bdc967fe71f70e9bab27579d
Henock Arega <Henock.Arega@student.hs-rm.de>
2018-11-24 12:38:37 +0100

    Implemented basic search algorithm.

--- abgabe.py
+++ abgabe.py
@@ -1,12 +1,37 @@
 from queue import Queue
-
 from node import Node
 
-def breadthfirst(queue, nodes):
+def breadthfirst(queue, node):
 
-
+	children = node.children()
+	for c in children:
+		breadthfirst(queue, c)
+		print(node._data)
 
 	return
+
+def search(start, goal):
+	from time import sleep
+
+	queue = [start]
+	reached = [start]
+
+	while queue:
+		state = queue.pop(0)
+		if state is goal:
+			return state
+
+		reached.append(state)
+		ex = state.expand()
+		newex = [s for s in ex if s not in reached]
+		print("state:", state, "\t>\tnewex:", newex, "\t>\tchildren:", state.children())
+		for e in newex:
+			queue.append(e)
+		a = 0
+		sleep(1)
+
+
+	return None
 
 if __name__ == '__main__':
 	root = Node(1)
@@ -21,22 +46,16 @@ if __name__ == '__main__':
 	root.add_child(two)
 	root.add_child(three)
 	root.add_child(four)
-
 	two.add_child(five)
-
 	three.add_child(six)
-
 	four.add_child(six)
-
 	five.add_child(root)
-
 	six.add_child(seven)
 	six.add_child(goal)
 
 	print(str(root))
 
 	queue = Queue(5)
-
-	breadthfirst(queue, root)
+	print(search(root, goal))
 
 
--- node.py
+++ node.py
@@ -4,9 +4,13 @@ class Node:
 	"""
 
 	def __init__(self, data, children=[]):
-		self.data = data
-		self.children = set()
+		self._data = None
+		self._children = set()
+		self.set_data(data)
 		self.add_child(nodes=children)
+
+	def set_data(self, data):
+		self._data = data
 
 	def add_child(self, node=None, nodes=None):
 		"""
@@ -15,13 +19,16 @@ class Node:
 		Node must be of type Node and also must not be None.
 		"""
 		lst = []
-		if node and type(node) is Node: lst.append(node)
+		if node and type(node) is Node:
+			lst.append(node)
 		if nodes and len(nodes) > 0:
 			for n in nodes:
-				if n and type(n) is Node: lst.append(n)
+				if n and type(n) is Node:
+					lst.append(n)
 
 		for n in lst:
-			if type(n) is Node: self.children.add(n)
+			if type(n) is Node:
+				self._children.add(n)
 
 	def insert_child(self, index, node):
 		"""
@@ -29,21 +36,42 @@ class Node:
 		Either just one child or a list of child nodes can be added.
 		Node must be of type Node and also must not be None.
 		"""
-		self.children
+		if index and node and type(node) is Node:
+			content = list(self._children)
+			self._children.clear()
+			content.insert(index, node)
+			for n in content: self._children.add(n)
 
 	def rm_child(self, node):
 		"""Remove a child node from this node."""
-		self.children.remove(node)
+		self._children.remove(node)
 
 	def next_child(self):
 		"""Get the next child """
-		for n in self.children: yield n
+		for n in self._children: yield n
+
+	def expand(self):
+		return list(self.children())
+
+	def children(self):
+		return self._children
 
 	def __repr__(self):
-		if len(self.children) == 0: return "Node({})".format(self.data)
-		return "Node({}, children={})".format(self.data, self.children)
+		# if len(self._children) == 0: return "Node({})".format(self._data)
+		# return "Node({}, children={})".format(self._data, self._children)
+		return self.__str__()
 
 	def __str__(self):
-		if len(self.children) == 0:
-			return "Node({})".format(self.data, self.children)
-		return "Node({}, {})".format(self.data, self.children).replace("set", "")
+		return "Node({})".format(self._data)
+
+	def __eq__(self, other):
+		return self.__hash__() == other.__hash__()
+
+	def __hash__(self):
+		return hash((self._data, str(self._children)))
+
+	def __gt__(self, other):
+		return self.__hash__() > other.__hash__()
+
+	def __lt__(self, other):
+		return self.__hash__() < other.__hash__()
