40d664a981cda18dfce708f0af8eda4faa3c53b2
Henock Arega <Henock.Arega@student.hs-rm.de>
2018-11-26 19:30:50 +0100

    Depthfirst is done and in some way cleaned up

--- puzzle/board.py
+++ puzzle/board.py
@@ -4,134 +4,129 @@ _max_depth = 50
 
 
 def breadthfirst(queue, expanded):
-    for e in expanded:
-        queue.append(e)
-    return queue
+	for e in expanded:
+		queue.append(e)
+	return queue
 
 
 def depthfirst(stack, expanded):
-    for e in expanded:
-        stack.insert(0, e)
-    return stack
+	for e in expanded:
+		stack.insert(0, e)
+	return stack
 
 
 def __depthfirst(start, goal):
-    queue = [start]
-    reached = [start]
-    depth = 0
+	queue = [start]
+	reached = [start]
+	depth = 0
 
-    while queue:
-        state = queue.pop(0)
+	while queue:
+		state = queue.pop(0)
 
-        if state._depth >= 6:
-            continue
+		if state._depth >= 6:
+			continue
 
-        if state == goal:
+		if state == goal:
 
-            path = [state]
-            while hasattr(path[-1], "_parent"):
-                path.append(path[-1].parent())
+			path = [state]
+			while hasattr(path[-1], "_parent"):
+				path.append(path[-1].parent())
 
-            return path[::-1], len(reached)
+			return path[::-1], len(reached)
 
-        reached.append(state)
-        children = state.expand()
-        depth += 1
-        newex = [s for s in children if s not in reached]
+		reached.append(state)
+		children = state.expand()
+		depth += 1
+		newex = [s for s in children if s not in reached]
 
-        queue = depthfirst(queue, newex)
+		queue = depthfirst(queue, newex)
 
-        print(reached[-1]._depth)
+		print(reached[-1]._depth)
 
-    return None
+	return None
+
+
+def path(node, reversed=False):
+	path = [node]
+	while path[-1].parent():
+		path.append(path[-1].parent())
+	return path[::-1] if not reversed else path
 
 
 def solve(start, goal, strategy=breadthfirst):
-    queue = [start]
-    reached = [start]
-    depth = 0
+	queue = [start]
+	reached = [start]
 
-    while queue:
-        state = queue.pop(0)
+	while queue:
+		state = queue.pop(0)
 
-        if state._depth >= 6:
-            continue
+		if state.depth() >= 6: continue
 
-        if state == goal:
+		if state == goal:
+			return path(state), len(reached)
 
-            path = [state]
-            while hasattr(path[-1], "_parent"):
-                path.append(path[-1].parent())
+		reached.append(state)
+		children = state.expand()
+		newex = [s for s in children if s not in reached]
 
-            return path[::-1], len(reached)
+		queue = strategy(queue, newex)
 
-        reached.append(state)
-        children = state.expand()
-        depth += 1
-        newex = [s for s in children if s not in reached]
-
-        # queue = strategy(queue, newex)
-        queue = strategy(queue, newex)
-
-        if len(reached) % 300 == 0:
-            print("len:", len(reached), ",", "dep:", depth, "# # " * 30, "\n")
-
-    return None
+	return None
 
 
 u_cost = 1
 
 if __name__ == '__main__':
-    init = [
-        [2, 8, 3],
-        [1, 6, 4],
-        [7, 0, 5],
-    ]
-    final = [
-        [1, 2, 3],
-        [8, 0, 4],
-        [7, 6, 5],
-    ]
+	init = [
+		[2, 8, 3],
+		[1, 6, 4],
+		[7, 0, 5],
+	]
+	final = [
+		[1, 2, 3],
+		[8, 0, 4],
+		[7, 6, 5],
+	]
 
-    root = Node(init)
-    goal = Node(final)
+	root = Node(init)
+	goal = Node(final)
 
-    result, path_len = solve(root, goal, strategy=depthfirst)
-    costs_total = len(result) * u_cost
+	result, path_len = solve(root, goal, strategy=depthfirst)
+	costs_total = len(result) * u_cost
 
-    print("init:\t\t", root)
-    print("goal:\t\t", goal)
-    print("result:\t\t")
+	print("init:\t\t", root)
+	print("goal:\t\t", goal)
+	print("result:\t\t")
 
-    for r in result:
-        for l in r._data:
-            print(l)
-        print("")
+	for r in result:
+		for l in r._data:
+			print(l)
+		print("")
 
-    print("")
+	print("")
 
-    print("cost p/p:\t", u_cost)
-    print("len:\t\t", len(result))
-    print("costs:\t\t", costs_total)
-    print("checked:\t", path_len)
+	print("cost p/p:\t", u_cost)
+	print("len:\t\t", len(result))
+	print("costs:\t\t", costs_total)
+	print("checked:\t", path_len)
 
-    print("\n", "--" * 40, "\n")
+	print("\n", "--" * 40, "\n")
 
-    result, path_len = solve(root, goal, strategy=breadthfirst)
-    costs_total = len(result) * u_cost
+	result, path_len = solve(root, goal, strategy=breadthfirst)
+	costs_total = len(result) * u_cost
 
-    print("init:\t\t", root)
-    print("goal:\t\t", goal)
-    print("result:\t\t")
+	print("init:\t\t", root)
+	print("goal:\t\t", goal)
+	print("result:\t\t")
 
-    for r in result:
-        for l in r._data:
-            print(l)
-        print("")
+	for r in result:
+		for l in r._data:
+			print(l)
+		print("")
 
-    print("")
+	print("")
 
-    print("cost p/p:\t", u_cost)
-    print("len:\t\t", len(result))
-    print("costs:\t\t", costs_total)
-    print("checked:\t", path_len)
+	print("cost p/p:\t", u_cost)
+	print("len:\t\t", len(result))
+	print("costs:\t\t", costs_total)
+	print("checked:\t", path_len)
--- puzzle/node.py
+++ puzzle/node.py
@@ -1,110 +1,112 @@
 class Node:
-    """
-    A simple node class. Can be used to create a tree-like structure.
-    """
-    _l, _r, _u, _d = "left", "right", "up", "down"
-    __keys = [_l, _u, _d, _r]
-    empty = 0
+	"""
+	A simple node class. Can be used to create a tree-like structure.
+	"""
+	_l, _r, _u, _d = "left", "right", "up", "down"
+	__keys = [_l, _u, _d, _r]
+	empty = 0
 
-    def __init__(self, data):
-        self._data = None
-        # self._children = { k:None for k in Node.__keys }
-        self._children = {}
-        self.set_data(data)
-        self.set_depth(0)
+	def __init__(self, data):
+		self._data = None
+		# self._children = { k:None for k in Node.__keys }
+		self._children = {}
+		self.set_data(data)
+		self.set_depth(0)
 
-    def set_data(self, data):
-        self._data = data
+	def set_data(self, data):
+		self._data = data
 
-    def expand(self):
-        data = self.children()
-        children = list(filter(lambda x: x is not None and x._data, list(self.children().values())))
-        for c in children:
-            c.set_parent(parent=self)
-            c.set_depth(self._depth + 1)
+	def depth(self): return self._depth
 
-        return children
+	def expand(self):
+		data = self.children()
+		children = list(filter(lambda x: x is not None and x._data, list(self.children().values())))
+		for c in children:
+			c.set_parent(parent=self)
+			c.set_depth(self._depth + 1)
 
-    def set_depth(self, depth):
-        self._depth = depth
+		return children
 
-    def set_children(self):
-        self._empty_x, self._empty_y = self.get_empty()
-        empty_x, empty_y = self._empty_x, self._empty_y
+	def set_depth(self, depth):
+		self._depth = depth
 
-        if empty_x > 0:
-            left = self.data_copy()
-            left[empty_y][empty_x], left[empty_y][empty_x - 1] = left[empty_y][empty_x - 1], left[empty_y][empty_x]
-        else:
-            left = None
+	def set_children(self):
+		self._empty_x, self._empty_y = self.get_empty()
+		empty_x, empty_y = self._empty_x, self._empty_y
 
-        if empty_x < len(self._data) - 1:
-            right = self.data_copy()
-            right[empty_y][empty_x], right[empty_y][empty_x + 1] = right[empty_y][empty_x + 1], right[empty_y][empty_x]
-        else:
-            right = None
+		if empty_x > 0:
+			left = self.data_copy()
+			left[empty_y][empty_x], left[empty_y][empty_x - 1] = left[empty_y][empty_x - 1], left[empty_y][empty_x]
+		else:
+			left = None
 
-        ## up/down
-        if empty_y > 0:
-            up = self.data_copy()
-            up[empty_y][empty_x], up[empty_y - 1][empty_x] = up[empty_y - 1][empty_x], up[empty_y][empty_x]
-        else:
-            up = None
+		if empty_x < len(self._data) - 1:
+			right = self.data_copy()
+			right[empty_y][empty_x], right[empty_y][empty_x + 1] = right[empty_y][empty_x + 1], right[empty_y][empty_x]
+		else:
+			right = None
 
-        if empty_y < len(self._data) - 1:
-            down = self.data_copy()
-            down[empty_y][empty_x], down[empty_y + 1][empty_x] = down[empty_y + 1][empty_x], down[empty_y][empty_x]
-        else:
-            down = None
+		## up/down
+		if empty_y > 0:
+			up = self.data_copy()
+			up[empty_y][empty_x], up[empty_y - 1][empty_x] = up[empty_y - 1][empty_x], up[empty_y][empty_x]
+		else:
+			up = None
 
-        self._children[self._l] = Node(left)
-        self._children[self._u] = Node(up)
-        self._children[self._d] = Node(down)
-        self._children[self._r] = Node(right)
+		if empty_y < len(self._data) - 1:
+			down = self.data_copy()
+			down[empty_y][empty_x], down[empty_y + 1][empty_x] = down[empty_y + 1][empty_x], down[empty_y][empty_x]
+		else:
+			down = None
 
-        return self._children
+		self._children[self._l] = Node(left)
+		self._children[self._u] = Node(up)
+		self._children[self._d] = Node(down)
+		self._children[self._r] = Node(right)
 
-    def get_empty(self):
-        for row in self._data:
-            if self.empty in row:
-                x, y = row.index(self.empty), self._data.index(row)
-                return x, y
+		return self._children
 
-    def data_copy(self):
-        copy = []
-        for lst in self._data:
-            copy.append([ele for ele in lst])
-        return copy
+	def get_empty(self):
+		for row in self._data:
+			if self.empty in row:
+				x, y = row.index(self.empty), self._data.index(row)
+				return x, y
 
-    def children(self):
-        return self.set_children()
+	def data_copy(self):
+		copy = []
+		for lst in self._data:
+			copy.append([ele for ele in lst])
+		return copy
 
-    def key(self):
-        return self._data
+	def children(self):
+		return self.set_children()
 
-    def parent(self):
-        return self._parent
+	def key(self):
+		return self._data
 
-    def set_parent(self, parent=None):
-        self._parent = parent
+	def parent(self):
+		return self._parent if hasattr(self, "_parent") else None
 
-    def set_move(self, direction):
-        self._move = str(direction) if type(direction) is str else direction
+	def set_parent(self, parent=None):
+		self._parent = parent
 
-    def __repr__(self):
-        return self.__str__()
+	def set_move(self, direction):
+		self._move = str(direction) if type(direction) is str else direction
 
-    def __str__(self):
-        return "Node({})".format(self._data)
+	def __repr__(self):
+		return self.__str__()
 
-    def __eq__(self, other):
-        return self.__hash__() == other.__hash__()
+	def __str__(self):
+		return "Node({})".format(self._data)
 
-    def __hash__(self):
-        return hash((str(self._data)))
+	def __eq__(self, other):
+		return self.__hash__() == other.__hash__()
 
-    def __gt__(self, other):
-        return self.__hash__() > other.__hash__()
+	def __hash__(self):
+		return hash((str(self._data)))
 
-    def __lt__(self, other):
-        return self.__hash__() < other.__hash__()
+	def __gt__(self, other):
+		return self.__hash__() > other.__hash__()
+
+	def __lt__(self, other):
+		return self.__hash__() < other.__hash__()
