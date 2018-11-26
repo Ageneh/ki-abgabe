a1f3ee29ab19cb579787ad2ec6ca1335a9618bbc
Henock Arega <Henock.Arega@student.hs-rm.de>
2018-11-26 17:01:14 +0100

    FIX: Depthfirst Max value was set tooo high

--- puzzle/board.py
+++ puzzle/board.py
@@ -1,87 +1,137 @@
 from puzzle.node import Node
 
-
 _max_depth = 50
 
 
-def breadthfirst(queue, expanded, **kargs):
-	for e in expanded:
-		queue.append(e)
-	return queue
+def breadthfirst(queue, expanded):
+    for e in expanded:
+        queue.append(e)
+    return queue
 
 
-def depthfirst(stack, expanded, depth=1, max_depth=_max_depth):
-	if depth < max_depth:
-		for e in expanded:
-			stack.insert(0, e)
-	else:
-		return breadthfirst(stack, expanded)
-	return stack
+def depthfirst(stack, expanded):
+    for e in expanded:
+        stack.insert(0, e)
+    return stack
+
+
+def __depthfirst(start, goal):
+    queue = [start]
+    reached = [start]
+    depth = 0
+
+    while queue:
+        state = queue.pop(0)
+
+        if state._depth >= 6:
+            continue
+
+        if state == goal:
+
+            path = [state]
+            while hasattr(path[-1], "_parent"):
+                path.append(path[-1].parent())
+
+            return path[::-1], len(reached)
+
+        reached.append(state)
+        children = state.expand()
+        depth += 1
+        newex = [s for s in children if s not in reached]
+
+        queue = depthfirst(queue, newex)
+
+        print(reached[-1]._depth)
+
+    return None
 
 
 def solve(start, goal, strategy=breadthfirst):
-	queue = [start]
-	reached = [start]
-	depth = 0
+    queue = [start]
+    reached = [start]
+    depth = 0
 
-	while queue:
-		state = queue.pop(0)
-		if state == goal:
+    while queue:
+        state = queue.pop(0)
 
-			path = [state]
-			while hasattr(path[-1], "_parent"):
-				path.append(path[-1].parent())
+        if state._depth >= 6:
+            continue
 
-			return path[::-1], len(reached)
+        if state == goal:
 
-		# print(state)
+            path = [state]
+            while hasattr(path[-1], "_parent"):
+                path.append(path[-1].parent())
 
-		reached.append(state)
-		children = state.expand()
-		depth += 1
-		newex = [s for s in children if s not in reached]
-		# newex = []
-		# for s in children:
-		# 	if s not in reached:
-		# 		newex.append(s)
+            return path[::-1], len(reached)
 
-		# queue = strategy(queue, newex)
-		queue = strategy(queue, newex, depth=depth, max_depth=40)
+        reached.append(state)
+        children = state.expand()
+        depth += 1
+        newex = [s for s in children if s not in reached]
 
-		# for s in newex:
-		# 	queue.append(s)
+        # queue = strategy(queue, newex)
+        queue = strategy(queue, newex)
 
-		if len(reached) % 300 == 0:
-			print("len:", len(reached), ",", "dep:", depth, "# # "*30, "\n")
+        if len(reached) % 300 == 0:
+            print("len:", len(reached), ",", "dep:", depth, "# # " * 30, "\n")
 
-	return None
+    return None
 
 
 u_cost = 1
 
-
 if __name__ == '__main__':
+    init = [
+        [2, 8, 3],
+        [1, 6, 4],
+        [7, 0, 5],
+    ]
+    final = [
+        [1, 2, 3],
+        [8, 0, 4],
+        [7, 6, 5],
+    ]
 
-	init = [
-		[2, 8, 3],
-		[1, 6, 4],
-		[7, 0, 5],
-	]
-	final = [
-		[1, 2, 3],
-		[8, 0, 4],
-		[7, 6, 5],
-	]
+    root = Node(init)
+    goal = Node(final)
 
-	root = Node(init)
-	goal = Node(final)
+    result, path_len = solve(root, goal, strategy=depthfirst)
+    costs_total = len(result) * u_cost
 
-	result, path_len = solve(root, goal, strategy=depthfirst)
-	costs_total = path_len * u_cost
+    print("init:\t\t", root)
+    print("goal:\t\t", goal)
+    print("result:\t\t")
 
-	print("init:\t\t", root)
-	print("goal:\t\t", goal)
-	print("result:\t\t", result)
-	print("cost p/p:\t", u_cost)
-	print("path len:\t", path_len)
-	print("costs:\t\t", costs_total)
+    for r in result:
+        for l in r._data:
+            print(l)
+        print("")
+
+    print("")
+
+    print("cost p/p:\t", u_cost)
+    print("len:\t\t", len(result))
+    print("costs:\t\t", costs_total)
+    print("checked:\t", path_len)
+
+    print("\n", "--" * 40, "\n")
+
+    result, path_len = solve(root, goal, strategy=breadthfirst)
+    costs_total = len(result) * u_cost
+
+    print("init:\t\t", root)
+    print("goal:\t\t", goal)
+    print("result:\t\t")
+
+    for r in result:
+        for l in r._data:
+            print(l)
+        print("")
+
+    print("")
+
+    print("cost p/p:\t", u_cost)
+    print("len:\t\t", len(result))
+    print("costs:\t\t", costs_total)
+    print("checked:\t", path_len)
--- searching/node.py
+++ searching/node.py
@@ -1,80 +1,80 @@
 class Node:
-	"""
-	A simple node class. Can be used to create a tree-like structure.
-	"""
+    """
+    A simple node class. Can be used to create a tree-like structure.
+    """
 
-	def __init__(self, data, children=[]):
-		self._data = None
-		self._children = set()
-		self.set_data(data)
-		self.add_child(nodes=children)
+    def __init__(self, data, children=[]):
+        self._data = None
+        self._children = set()
+        self.set_data(data)
+        self.add_child(nodes=children)
 
-	def set_data(self, data):
-		self._data = data
+    def set_data(self, data):
+        self._data = data
 
-	def add_child(self, node=None, nodes=None):
-		"""
-		Add a new child node to this node.
-		Either just one child or a list of child nodes can be added.
-		Node must be of type Node and also must not be None.
-		"""
-		lst = []
-		if node and type(node) is Node:
-			lst.append(node)
-		if nodes and len(nodes) > 0:
-			for n in nodes:
-				if n and type(n) is Node:
-					lst.append(n)
+    def add_child(self, node=None, nodes=None):
+        """
+        Add a new child node to this node.
+        Either just one child or a list of child nodes can be added.
+        Node must be of type Node and also must not be None.
+        """
+        lst = []
+        if node and type(node) is Node:
+            lst.append(node)
+        if nodes and len(nodes) > 0:
+            for n in nodes:
+                if n and type(n) is Node:
+                    lst.append(n)
 
-		for n in lst:
-			if type(n) is Node:
-				self._children.add(n)
+        for n in lst:
+            if type(n) is Node:
+                self._children.add(n)
 
-	def insert_child(self, index, node):
-		"""
-		Add a new child node to this node.
-		Either just one child or a list of child nodes can be added.
-		Node must be of type Node and also must not be None.
-		"""
-		if index and node and type(node) is Node:
-			content = list(self._children)
-			self._children.clear()
-			content.insert(index, node)
-			for n in content: self._children.add(n)
+    def insert_child(self, index, node):
+        """
+        Add a new child node to this node.
+        Either just one child or a list of child nodes can be added.
+        Node must be of type Node and also must not be None.
+        """
+        if index and node and type(node) is Node:
+            content = list(self._children)
+            self._children.clear()
+            content.insert(index, node)
+            for n in content: self._children.add(n)
 
-	def rm_child(self, node):
-		"""Remove a child node from this node."""
-		self._children.remove(node)
+    def rm_child(self, node):
+        """Remove a child node from this node."""
+        self._children.remove(node)
 
-	def next_child(self):
-		"""Get the next child """
-		for n in self._children: yield n
+    def next_child(self):
+        """Get the next child """
+        for n in self._children: yield n
 
-	def expand(self):
-		return list(self.children())
+    def expand(self):
+        return list(self.children())
 
-	def children(self):
-		return self._children
+    def children(self):
+        return self._children
 
-	def key(self):
-		return self._data
+    def key(self):
+        return self._data
 
-	def __repr__(self):
-		# if len(self._children) == 0: return "Node({})".format(self._data)
-		# return "Node({}, children={})".format(self._data, self._children)
-		return self.__str__()
+    def __repr__(self):
+        # if len(self._children) == 0: return "Node({})".format(self._data)
+        # return "Node({}, children={})".format(self._data, self._children)
+        return self.__str__()
 
-	def __str__(self):
-		return "Node({})".format(self._data)
+    def __str__(self):
+        return "Node({})".format(self._data)
 
-	def __eq__(self, other):
-		return self.__hash__() == other.__hash__()
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
 
-	def __hash__(self):
-		return hash((self._data, str(self._children)))
+    def __hash__(self):
+        return hash((self._data, str(self._children)))
 
-	def __gt__(self, other):
-		return self.__hash__() > other.__hash__()
+    def __gt__(self, other):
+        return self.__hash__() > other.__hash__()
 
-	def __lt__(self, other):
-		return self.__hash__() < other.__hash__()
+    def __lt__(self, other):
+        return self.__hash__() < other.__hash__()
