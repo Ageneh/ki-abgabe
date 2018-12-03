db8b0de6960df0923f33c57b29a29b35b4036b54
Henock Arega <Henock.Arega@student.hs-rm.de>
2018-12-03 17:24:47 +0100

    Added logging.

--- puzzle/a_star.py
+++ puzzle/a_star.py
@@ -18,9 +18,11 @@ def h_manhattan(node, goal):
 class AStar:
 
     @staticmethod
-    def solve(start, goal, heuristic=h_manhattan, **kwargs):
+    def solve(start, goal, heuristic=h_manhattan, w_costs=1, **kwargs):
         p_queue = [(0, start)]
         reached = [start]
+        current_costs = {start: 0} # total costs to each node
+
 
         while p_queue:
             cost, state = p_queue.pop(0)
@@ -30,8 +32,14 @@ class AStar:
 
             reached.append(state)
             children = state.expand()
-            newex = [(cost + heuristic(s, goal), s) for s in children if s not in reached]
-            p_queue.extend(newex)
+
+            for child in children:
+                n_cost = current_costs[state] + w_costs
+                if child not in current_costs or n_cost < current_costs[child]:
+                    current_costs[child] = n_cost
+                    prio = n_cost + heuristic(child, goal)
+                    p_queue.append((prio, child))
+
             p_queue = sorted(p_queue, key=lambda x: x[0])
 
         return None
--- puzzle/test.py
+++ puzzle/test.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     goal = Node(final, goal=True)
 
     alg = AStar()
-    result, path_len = alg.solve(root, goal, strategy=depthfirst, heuristic=h_hamilton)
+    result, path_len = alg.solve(root, goal, strategy=depthfirst, heuristic=h_manhattan)
     costs_total = len(result) * u_cost
 
     print("init:\t\t", root)
